03fe4fb6d00471fb925061b064f92644e933a124
jessynakyeonglee <leena@ssfs.org>
2020-03-31 21:12:08 +0900

    3/31/20

--- src/TwoDArrays.java
+++ src/TwoDArrays.java
@@ -2,7 +2,7 @@ public class TwoDArrays {
     public static int maxValue (int [][]list){
         int max = Integer.MIN_VALUE;
         for (int x=0;x<list.length;x++){
-            for (int y=0;y< list.length;y++){
+            for (int y=0;y< list[1].length;y++){
                 if (list[x][y]>max) {
                     max = list[x][y];
                 }
@@ -14,11 +14,18 @@ public class TwoDArrays {
     public static int sumAll (int [][]list) {
         int output = 0;
         for (int x = 0; x < list.length; x++) {
-            for (int y = 0; y < list.length; y++) {
+            for (int y = 0; y < list[1].length; y++) {
                 output += list[x][y];
             }
         }
         return output;
     }
-
+    public static boolean isSquare (int [][]arr){
+        if (arr[1].length!=arr.length){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
 }
